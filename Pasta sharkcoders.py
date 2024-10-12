ef7d1596122b59de84cd892b184e2f962cf4648a
gilpmoura <gilmoura26082011@gmail.com>
2024-10-12 12:31:21 +0100

    Add files via upload

---
+++ Pasta sharkcoders.py
@@ -0,0 +1,33 @@
+SHARK CODERS
+11:49
+#print("ola") #eu também posso comentar aqui
+#se eu fizer uma função print, a consola vai escrever aquilo que pus nos parêntesis
+
+
+# ----------------- COMENTÁRIOS --------------------
+# o comentário é uma parte do código que não vai ser lida pelo programa, ou seja:
+# podemos escrever anotações no código sem que elas perturbem o seu funcionamento
+# devido à SINTAXE do python
+# PYTHON -> linguagem de programação
+#               |
+#               v
+#           gramática --> SINTAXE, ou seja, como a#           gramática --> SINTAXE, ou seja, como a linguagem FUNCIONA
+#
+# # IMPORTANTE: SEMPRE que queremos comentar alguma coisa, temos de colocar [#] ANTES e em TODAS AS LINHAS DE COMENTÁRIO
+#
+# palavra= "hamburger" # MAIS DOIS TIPOS DE DADOS: VARIÁVEL (que armazena um dado); e STRING (texto)
+# # quando temos uma STRING podemos "copiar e colar" o que está dentro de aspas
+#
+# print("hamburger")
+# print(palavra) #podemos usar as variáveis (texto branco) em qualquer parte do código#-------------------------------VARIÁVEIS---------------------------------------
+#
+# # uma variável contém um valor que pode mudar, como se fosse uma caixa em que podemos pôr várias coisas (e alterá-las)
+# #REGRAS:
+# # NÃO PODEM começar com números!
+# print(5) #número fica AZUL
+# # podemos fazer contas com NÚMEROS
+# # se uma VARIÁVEL começar com um NÚMERO, o código vai AUTOMATICAMENTE pensar que estamos a tratar de um NÚMERO:
+# porcento=5 #NO ENTANTO, podemos definir uma VARIÁVEL com apenas um número
+# # isto acontece porque estamos a armazenar o valor de 5 na variável, ou seja, PORCENTO=5, MAS 5 NÃO PODE SER IGUAL A
+# #                                                                                                      PORCENTO
+# evr-shya-otd
